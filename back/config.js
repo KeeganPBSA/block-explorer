920dbb1e4b66557c33669fb2682766f301651790
Raymond Tyler Wall <git@rtwall.ca>
2018-01-21 18:53:28 -0400

    gh-1 add configuration system and glue manifest
    - based on a trimmed down version of Frame

---
+++ back/config.js
@@ -0,0 +1,41 @@
+'use strict';
+const Confidence = require('confidence');
+const Dotenv = require('dotenv');
+
+
+Dotenv.config({ silent: true });
+
+const criteria = {
+    env: process.env.NODE_ENV
+};
+
+
+const config = {
+    projectName: 'Peerplays Block Explorer Backend',
+    logLevel: "info",
+    port: { api: 8000 },
+    hapiMongoModels: {
+        mongodb: {
+            uri: {
+                $filter: 'env',
+                production: process.env.MONGODB_URI,
+                test: 'mongodb://localhost:27017/pbsa',
+                $default: 'mongodb://localhost:27017/pbsa'
+            }
+        },
+        autoIndex: true
+    },
+};
+
+
+const store = new Confidence.Store(config);
+
+
+exports.get = function (key) {
+    return store.get(key, criteria);
+};
+
+
+exports.meta = function (key) {
+    return store.meta(key, criteria);
+};
