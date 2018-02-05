f54e9125ca7f98fcdaba199917112e7eb14ed452
Raymond Tyler Wall <git@rtwall.ca>
2018-02-05 09:20:44 -0400

    gh-7 update /api/blocks to return transaction count
    - reduce CORS security short term
    - update default block sort ordering

--- back/server/api/blocks.js
+++ back/server/api/blocks.js
@@ -2,23 +2,28 @@
 
 const Block = require('../models/Block');
 const Joi = require('joi');
+const _ = require('lodash');
+
 
 exports.register = function (server, options) {
   server.route({
       method: 'GET',
       path: '/blocks',
       config: {
+          cors: {
+              origin: 'ignore'
+          },
           validate: {
               query: {
-                  sort: Joi.string().default('_id'),
+                  sort: Joi.string().default('-_id'),
                   "page[offset]": Joi.number().positive().default(0),
                   "page[limit]": Joi.number().positive().max(100).default(20)
               }
           }
       },
       handler: async function (request, h) {
-          var query = {};
-          var options = {
+          const query = {};
+          let options = {
               sort: request.query.sort,
               populate: 'author',
               lean: true,
@@ -26,7 +31,12 @@ exports.register = function (server, options) {
               limit: request.query['page[limit]']
           };
           // We are hiding metadata here that could be useful later
-          return { blocks: (await Block.paginate(query, options)).docs };
+          let result = (await Block.paginate(query, options)).docs;
+          let blocks = _.map(result, function(item) {
+              item.transactions = item.transactions.length;
+              return item;
+          });
+          return {blocks: blocks};
 
       }
   });
