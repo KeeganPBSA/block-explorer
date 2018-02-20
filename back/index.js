22e1d0806ec253b0311aca66705c7d09628e94cf
Raymond Tyler Wall <git@rtwall.ca>
2018-02-19 20:27:30 -0400

    gh-9 add basic WebSocket server to publish Blocks on an interval
    This will be updated in a future task.

--- back/index.js
+++ back/index.js
@@ -1,12 +1,23 @@
 'use strict';
 const Config = require('./config');
 const Glue = require('glue');
+const Hapi = require('hapi');
+const Log = require('./server/utils/logger');
 const Manifest = require('./manifest');
 const Mongoose = require('mongoose');
-const Log = require('./server/utils/logger');
+const Nes = require('nes');
 
 
 exports.deployment = async (start) => {
+
+    let mongoUri =  Config.get('/mongodb/uri');
+    Mongoose.connect(mongoUri, {}).then(() => {
+        Log.info(`Connected to ${mongoUri}`);
+    },
+    err => {
+        Log.error(err);
+    });
+
     const manifest = Manifest.get('/', process.env);
     const server = await Glue.compose(manifest, { relativeTo: __dirname });
     await server.initialize();
@@ -15,14 +26,22 @@ exports.deployment = async (start) => {
     }
     await server.start();
 
-    let mongoUri =  Config.get('/mongodb/uri');
+    const socketServer = new Hapi.Server({ port: 8126 });
+    await socketServer.register(Nes);
+    socketServer.subscription('/blocks');
+    await socketServer.start();
 
-    Mongoose.connect(mongoUri, {}).then(() => {
-        Log.info(`Connected to ${mongoUri}`);
-    },
-    err => {
-        Log.error(err);
-    });
+    Log.info(`Websocket port is ${socketServer.info.port}`);
+
+    const publish = async function () {
+        Log.debug('publishing block update to socket');
+        const options = {url: '/api/blocks'}
+        let blocks = (await server.inject('/api/blocks')).result;
+        socketServer.publish('/blocks', blocks);
+    }
+
+    setInterval(publish, 5000);
+
 
     Log.info(`Server started at ${server.info.uri}`);
     return server;
